8f81f97108ee8322f02c9b2ad736a0361cfbb927
Myke Phillips <info@thefraternity.co.uk>
2018-08-30 07:22:09 +0100

    Added player model. Added segues. Passed data through segue.

--- SwooshApp/Controller/LeagueVC.swift
+++ SwooshApp/Controller/LeagueVC.swift
@@ -10,18 +10,56 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
 
-        
+
     }
 
-    
-    @IBAction func onNextTapped(_ sender: UIButton) {
 
+    @IBAction func onNextTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
+
+
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLegue(playerType: "Mens")
+
+    }
+    
+
+    @IBAction func onWoemsTapped(_ sender: Any) {
+        selectLegue(playerType: "Womens")
+
+    }
+
+
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLegue(playerType: "Coed")
+
+    }
+
+
+    func selectLegue(playerType: String) {
+        player.desiredLeague = playerType
+        nextBtn.isEnabled = true
+
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+            
+        }
+    }
+
     
 
 
---
+++ SwooshApp/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  SwooshApp
+//
+//  Created by Myke Phillips on 30/08/2018.
+//  Copyright © 2018 Myke Phillips. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+
+
+}
